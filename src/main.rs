bf8f942cb08274111315a2d8eb24dacc64ef88cd
Jethro Djan <jthryeboah@gmail.com>
2025-04-03 00:48:01 +0000

    Configure postgres db in Docker container

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
 use iced::{Element, Task};
 
+use sqlx::postgres::PgPool;
+use std::sync::Arc;
+use std::env;
+use dotenv::dotenv;
+
 use screen::login;
 use screen::main_app;
 
 fn main() -> iced::Result {
+    let rt = tokio::runtime::Runtime::new().expect("Failed to create runtime");
+    let handle = rt.handle().clone();
+
     iced::application(
         RidgeExamTool::title,
         RidgeExamTool::update,
         RidgeExamTool::view,
     )
     .centered()
-    .run_with(move || RidgeExamTool::new())
+    .antialiasing(true)
+    .run_with(|| (RidgeExamTool::new(handle)))
 }
 
 struct RidgeExamTool {
     screen: Screen,
+    db_pool: Option<Arc<PgPool>>,
+    error: Option<Error>,
+    runtime_handle: tokio::runtime::Handle,
 }
 
 #[derive(Debug)]
@@ -27,15 +39,27 @@ pub enum Screen {
 pub enum Message {
     OpenLoginView(login::Message),
     OpenMainAppView(main_app::Message),
+    OpenDatabase(Result<PgPool, Error>),
 }
 
 impl RidgeExamTool {
-    pub fn new() -> (Self, Task<Message>) {
+    pub fn new(handle: tokio::runtime::Handle) -> (Self, Task<Message>) {
+        dotenv().ok();
+        let database_url = env::var("DATABASE_URL")
+            .unwrap_or_else(|_| "postgres://postgres:mysecretpassword@localhost:5432/examtool-db");
         (
             Self {
                 screen: Screen::LoginView(login::Login::new()),
+                db_pool: None,
+                error: None,
+                runtime_handle: handle.clone(),
             },
-            Task::none(),
+            Task::perform(
+                async move {
+                    handle.block_on(db::connect(&database_url))
+                },
+                Message::OpenDatabase,
+            ),
         )
     }
 
@@ -46,17 +70,35 @@ impl RidgeExamTool {
     pub fn update(&mut self, message: Message) -> Task<Message> {
         match message {
             Message::OpenLoginView(message) => {
-                let login = login::Login::new();
-                self.screen = Screen::LoginView(login);
+                let mut login = login::Login::new();
+                self.screen = Screen::LoginView(login.clone());
 
-                Task::none()
+                let action = login.update(message);
+
+                match action {
+                    login::Action::None => Task::none(),
+                    login::Action::Login => {
+                        let (main_app, task) = main_app::MainApp::new();
+
+                        self.screen = Screen::MainAppView(main_app);
+
+                        task.map(Message::OpenMainAppView)
+                    }
+                }
             }
             Message::OpenMainAppView(message) => {
-                let main_app = main_app::MainApp::new();
+                let (main_app, task) = main_app::MainApp::new();
+
                 self.screen = Screen::MainAppView(main_app);
 
                 Task::none()
             }
+            Message::OpenDatabase(Ok(pool)) => {
+                self.db_pool = Some(pool.into());
+
+                Task::none()
+            } 
+            Message::OpenDatabase(Err(_)) => Task::none(),
         }
     }
 
@@ -70,8 +112,8 @@ impl RidgeExamTool {
 
 mod screen {
     pub mod sidebar {
-        use iced::{Element, Task};
         use iced::widget::text;
+        use iced::{Element, Task};
 
         #[derive(Debug, Clone)]
         pub enum Message {
@@ -130,16 +172,10 @@ mod screen {
     }
 
     pub mod login {
-        use iced::Task;
-        use iced::widget::{
-            Column, button, column, container, image, text, text_input,
-            Container,
-        };
-        use iced::{Center, Element, Length, Theme, Background, Color, Border, border};
+        use iced::widget::{Column, Container, button, column, container, text, text_input};
+        use iced::{Background, Border, Center, Color, Element, Length, Theme, border};
 
-        use crate::RidgeExamTool;
-        use crate::Screen;
-        use crate::main_app;
+        use crate::icon::ridge;
 
         pub const RIDGE_SCHOOL: &str = "Ridge SHS";
 
@@ -154,6 +190,11 @@ mod screen {
             LoginButtonPressed,
             UsernameInputChanged(String),
             PasswordInputChanged(String),
+        }
+
+        pub enum Action {
+            None,
+            Login,
         }
 
         pub fn login_box_style(theme: &Theme) -> container::Style {
@@ -184,12 +225,12 @@ mod screen {
         }
 
         fn login_screen<'a>(state: &Login) -> Element<'a, Message> {
-            let ridge_logo = image(image::Handle::from_path(format!(
-                "{}/assets/images/Ridge_School_Kumasi_Logo.png",
-                env!("CARGO_MANIFEST_DIR")
-            )))
-            .height(60);
-            let school_info = column![ridge_logo, text!("{}", RIDGE_SCHOOL).size(35)]
+            // let ridge_logo = image(image::Handle::from_path(format!(
+            //     "{}/assets/images/Ridge_School_Kumasi_Logo.png",
+            //     env!("CARGO_MANIFEST_DIR")
+            // )))
+            // .height(60);
+            let school_info = column![ridge(), text!("{}", RIDGE_SCHOOL).size(35)]
                 .align_x(Center)
                 .spacing(5);
 
@@ -233,22 +274,19 @@ mod screen {
                 }
             }
 
-            pub fn update(&mut self, message: Message) -> Task<Message> {
+            pub fn update(&mut self, message: Message) -> Action {
                 match message {
                     Message::UsernameInputChanged(username) => {
                         self.username = username;
-                        Task::none()
+
+                        Action::None
                     }
                     Message::PasswordInputChanged(password) => {
                         self.password = password;
-                        Task::none()
-                    }
-                    Message::LoginButtonPressed => {
-                        let (mut main_state, command) = RidgeExamTool::new();
-                        main_state.screen = Screen::MainAppView(main_app::MainApp {});
 
-                        Task::none()
+                        Action::None
                     }
+                    Message::LoginButtonPressed => Action::Login,
                 }
             }
 
@@ -256,12 +294,11 @@ mod screen {
                 login_screen(self)
             }
         }
-
     }
 
     pub mod main_app {
-        use iced::{Task, Element};
-        use iced::widget::{text, };
+        use iced::widget::text;
+        use iced::{Element, Task};
 
         #[derive(Debug, Clone)]
         pub struct MainApp {}
@@ -270,8 +307,8 @@ mod screen {
         pub enum Message {}
 
         impl MainApp {
-            pub fn new() -> Self {
-                Self {}
+            pub fn new() -> (Self, Task<Message>) {
+                (Self {}, Task::none())
             }
 
             pub fn update(&mut self, message: Message) -> Task<Message> {
@@ -318,6 +355,41 @@ pub mod icon {
             "{}/assets/images/Ridge_School_Kumasi_Logo.png",
             env!("CARGO_MANIFEST_DIR")
         )))
-        .height(60)
+        .height(80)
+    }
+}
+
+pub mod db {
+    use tokio::runtime::Runtime;
+    use sqlx::postgres::PgPoolOptions;
+    use sqlx::PgPool;
+
+    use std::sync::Arc;
+    use std::env;
+
+    use crate::Error;
+
+    pub async fn connect(connection_str: &str) -> Result<PgPool, Error> {
+        let pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(connection_str)
+            .await
+            .map_err(|error| Error::from(error))?;
+
+        Ok(pool)
+
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Error {
+    DbError,
+}
+
+impl From<sqlx::Error> for Error {
+    fn from(error: sqlx::Error) -> Error {
+        dbg!(error);
+
+        Error::DbError
     }
 }
